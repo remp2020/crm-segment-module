5b99744799f7ef282de316eb2cea8147540dfd2c
Dominik Skerhak <dominik.skerhak@gmail.com>
2020-06-17 22:24:42 +0200

    Fix doubled slash in crmHost in Scenarios and Segments
    remp/crm#1324

--- src/presenters/StoredSegmentsPresenter.php
+++ src/presenters/StoredSegmentsPresenter.php
@@ -224,7 +224,7 @@ class StoredSegmentsPresenter extends AdminPresenter
 
     public function renderEmbed($id)
     {
-        $this->template->crmHost = $this->getHttpRequest()->getUrl()->getBaseUrl() . "/api/v1";
+        $this->template->crmHost = $this->getHttpRequest()->getUrl()->getBaseUrl() . "api/v1";
         $this->template->segmentAuth = 'Bearer ' . $this->accessToken->getToken($this->getHttpRequest());
 
         $segment = $this->segmentsRepository->find($id);
